0726769a351965199bcdf0aa1d2e638d7956b16c
Michael Khaimov <khaimovmr@haimov.ru>
2013-09-03 03:07:14 +0300

    0.8

--- static/js/general.js
+++ static/js/general.js
@@ -1,36 +1,68 @@
+var dzStartX = 20.84; // 0 x
+var dzStartY = 20.87; // 0 y
+var dzEndX = 234.336; // 152 x
+var dzEndY = 234.35; // 152 y
+var dzQX = (dzEndX - dzStartX) / 152; // quadrant mesurement by X axis
+var dzQY = (dzEndY - dzStartY) / 152; // quadrant measurement by Y axis
 var dayzMap;
+var mapProjection;
 
-$(document).ready(function(){
-    // Normalizes the coords that tiles repeat across the x axis (horizontally)
-    // like the standard Google map tiles.
-    function getNormalizedCoord(coord, zoom) {
-        var y = coord.y;
-        var x = coord.x;
+// Normalizes the coords that tiles repeat across the x axis (horizontally)
+// like the standard Google map tiles.
+function getNormalizedCoord(coord, zoom) {
+    var y = coord.y;
+    var x = coord.x;
+    // tile range in one direction range is dependent on zoom level
+    // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
+    var tileRange = 1 << zoom;
 
-        // tile range in one direction range is dependent on zoom level
-        // 0 = 1 tile, 1 = 2 tiles, 2 = 4 tiles, 3 = 8 tiles, etc
-        var tileRange = 1 << zoom;
-
-        // don't repeat across y-axis (vertically)
-        if (y < 0 || y >= tileRange) {
-            return null;
-        }
-
-        // repeat across x-axis
-        if (x < 0 || x >= tileRange) {
-            return null; //x = (x % tileRange + tileRange) % tileRange;
-        }
-
-        return {
-            x: x,
-            y: y
-        };
+    // don't repeat across y-axis (vertically)
+    if (y < 0 || y >= tileRange) {
+        return null;
+    }
+    
+    // repeat across x-axis
+    if (x < 0 || x >= tileRange) {
+        return null; //x = (x % tileRange + tileRange) % tileRange;
     }
 
+    return {
+        x: x,
+        y: y
+    };
+}
+
+function makePlayer(name, x, y) {
+    latLng = getLatLng(x, y);
+
+    if (latLng) {
+        var player = new google.maps.Marker({
+            position: latLng,
+            title: name
+        });
+        player.setMap(dayzMap);
+        return player;
+    }
+    else {
+        console.log('Error: map projection is not initialized yet.');
+    }
+}
+
+function getLatLng(x, y) {
+    if (mapProjection) {
+        var point = new google.maps.Point(x, y);
+        return mapProjection.fromPointToLatLng(point);
+    }
+    else {
+        return false;
+    }
+}
+
+$(document).ready(function(){
     var mapTypeOptions = {
         center: new google.maps.LatLng(0, 0),
         maxZoom: 6,
-        minZoom: 2,
+        minZoom: 1,
         zoom: 2,
         radius: 10000,
         tileSize: new google.maps.Size(256, 256),
@@ -62,44 +94,9 @@ $(document).ready(function(){
     dayzMap = new google.maps.Map(document.getElementById('map'), mapOptions);
     dayzMap.mapTypes.set('dayz_chernarus', chernarusMapType);
     dayzMap.setMapTypeId('dayz_chernarus');
-    alert(2);
-        
-    function makePlayer(name, latLng) {
-        var player = new google.maps.Marker({
-            position: latLng,
-            title: name
-        });
-        player.setMap(dayzMap);
-    }
-    
-    function px2deg(px) {
-        var rad = Math.atan(px / Math.sqrt(152000 - px^2))
-        return rad * 180/ Math.PI;
-    }
-    
-    function genDayZCoords(a, b) {
-        a -= 4000;
-        b *= -1 / 5.8
-        b += 4000;
-        return [a, b];
-    }
-    
-    var xD = 0;
-    var yD = 0;
-    var y = 0;
-    var latLng = 0;
-    
-    for (var x = 0; x < 1000; x += 1)
-    {
-        y = x;
-        dayZCoords = genDayZCoords(x, y);
-        xD = dayZCoords[0];
-        yD = dayZCoords[1];
-        xDeg = px2deg(xD);
-        yDeg = px2deg(yD);
-        latLng = new google.maps.LatLng(yDeg, xDeg);
-        makePlayer('Player [' + x + ' x ' + y + ']', latLng);
-    }
+    google.maps.event.addListener(dayzMap, 'projection_changed', function() {
+        mapProjection = dayzMap.getProjection();
+    });
 });
 
 
